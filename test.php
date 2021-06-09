bd8abb423f74900fa28658f5b26ada41dc833674
Seongyeonju <raumstreasure@gmail.com>
2021-06-09 11:08:44 +0900

    glt test입니다.... 올라가랏

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+include_once('./_common.php');
+$arr=array();
+$arr[]=array("4","a");
+$arr[]=array("3","b");
+$arr[]=array("4","a");
+array_unique($arr);
+asort($arr);
+//$arr=asort(array_unique($arr));
+echo $arr[0][0];
+
+$tester = "tdstdt";
+echo $tester;
+git test;
+git test1;
+?>
+
